b68ddb00a31a49aad5b7df54c40c2011723a9b5c
bernadoganer <101328722+bernadoganer@users.noreply.github.com>
2022-09-18 19:10:26 +0300

    ödev
    ödev

---
+++ src/kodlamaIoDevsProject/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.ProgrammingLanguages.Rules;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Core.Application.Pipelines.Validation;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Reflection;
+
+namespace Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<ProgrammingLanguageBusinessRules>();
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+
+        }
+    }
+}
